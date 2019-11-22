aeb75a48b08e60353be5e8df9a7934032a41b203
kmh339 <kmh339@gmail.com>
2019-11-22 18:23:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/network/UsersAdapter.java
@@ -0,0 +1,62 @@
+package com.example.network;
+
+import android.app.Activity;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.CustomViewHolder> {
+
+    private ArrayList<PersonalData> mList = null;
+    private Activity context = null;
+
+
+    public UsersAdapter(Activity context, ArrayList<PersonalData> list) {
+        this.context = context;
+        this.mList = list;
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+        protected TextView id;
+        protected TextView name;
+        protected TextView address;
+
+
+        public CustomViewHolder(View view) {
+            super(view);
+            this.id = (TextView) view.findViewById(R.id.textView_list_id);
+            this.name = (TextView) view.findViewById(R.id.textView_list_name);
+            this.address = (TextView) view.findViewById(R.id.textView_list_address);
+        }
+    }
+
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list, null);
+        CustomViewHolder viewHolder = new CustomViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder viewholder, int position) {
+
+        viewholder.id.setText(mList.get(position).getMember_id());
+        viewholder.name.setText(mList.get(position).getMember_name());
+        viewholder.address.setText(mList.get(position).getMember_address());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != mList ? mList.size() : 0);
+    }
+
+}
+
